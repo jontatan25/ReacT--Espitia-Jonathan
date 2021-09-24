1cbca188bed997c292d7d06302cc38a7cfd73a3a
Jonathan <jhonn.photo@gmail.com>
2021-09-24 18:58:03 -0500

    Creando NavBar

---
+++ apptienda/src/components/NavBar.js
@@ -0,0 +1,22 @@
+
+function NavBar() {
+    return (
+      <>
+    <header>
+        <h1>Tienda de Retoque Fotográfico</h1>
+        <nav>
+        <ul class="nav" >
+            <li>Retoque</li>
+            <li>Edición</li>
+            <li>Montaje</li>
+            <li>Mejora</li>
+            <li>Restauración</li>
+        </ul>
+        </nav>
+    </header>
+      </>
+    );
+  }
+  
+  export default NavBar;
+  
