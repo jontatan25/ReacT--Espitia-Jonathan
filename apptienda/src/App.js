07542d73055a4b7c6816cc334cb6e33c2296d809
Jonathan <jhonn.photo@gmail.com>
2021-10-04 10:16:47 -0500

    Creando ItemListContainer - Contador

--- apptienda/src/App.js
+++ apptienda/src/App.js
@@ -3,12 +3,14 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import  "./components/ItemListContainer/ItemListContainer.css"
 import React from 'react'
+import ItemCount from "./components/ItemCount";
 
 const App = () => {
   return (
     <div>
        <NavBar />
        <ItemListContainer nombre= "Pedro" apellido= "Sanchez"/>
+       <ItemCount/>
     </div>
   )
 }
